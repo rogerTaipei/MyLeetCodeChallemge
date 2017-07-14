915de7c90e0c05df1f9747193aaeeb16f90cb082
roger <bipartite20567@gmail.com>
2017-07-15 02:57:12 +0800

    Title : Create Project
    Description : Create project and add three lib

---
+++ LeetCodeChallenge/CommonBase/EntryBase.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommonBase
+{
+    public class EntryBase
+    {
+        protected List<ICommonFunc> Funcs = null;
+
+        public EntryBase()
+        {
+             Funcs = new List<ICommonFunc>();
+        }
+
+        public List<ICommonFunc> GetAllFunc()
+        {
+            return Funcs;
+        }
+
+        public List<string> GetAllFuncTitle()
+        {
+            return Funcs.Select(_ => _.GetFuncTitle()).ToList();
+        }
+
+        public List<string> GetAllFuncDesc()
+        {
+            return Funcs.Select(_ => _.GetFuncDescription()).ToList();
+        }
+    }
+}
---
+++ LeetCodeChallenge/LeetCodeEasy/Entry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeEasy
+{
+    public class Entry : CommonBase.EntryBase
+    {
+        public Entry()
+            :base()
+        {
+            integerReverse = new IntegerReverse();
+
+            Funcs.Add(integerReverse);
+        }
+
+
+
+        IntegerReverse integerReverse;
+    }
+}
