633193b02fc331fc68d07a93d047592f2ed98f46
RogerNeihu <bipartite20567@gmail.com>
2020-07-22 01:35:33 +0800

    Update solution

--- LeetCodeChallenge/LeetCodeMedium/MinCoinChallenge.cs
+++ LeetCodeChallenge/LeetCodeMedium/MinCoinChallenge.cs
@@ -52,6 +52,54 @@ namespace LeetCodeMedium
             return dpAmount[amount];
         }
 
+        public int CoinChange2(int[] coins, int amount)
+        {
+            int[] dpAmount = new int[amount + 1];
+            dpAmount[0] = 0;
+
+            //  0   1   2   3   4   5   6   7   8   9   10  11
+            //  0   1
+
+            //  0   1   2
+            //  0   1   1
+
+            //  0   1   1   3
+            //  0   1   1   2
+
+            //  0   1   1   2   4
+            //  0   1   1   2   2
+
+            //  0   1   1   2   2   3
+            //  0   1   1   2   2   3
+            //  0   1   1   2   2   1
+            for (int currentAmout = 1; currentAmout <= amount; ++currentAmout)
+            {
+                dpAmount[currentAmout] = int.MaxValue;
+                for (int coinIndex = 0; coinIndex < coins.Length; ++coinIndex)
+                {
+                    bool isHasSolution = false;
+                    int coin = coins[coinIndex];
+                    if (coin > currentAmout)
+                    {
+                        break;
+                    }
+
+                    else if (coin <= currentAmout)
+                    {
+                        int value1 = dpAmount[currentAmout - coin] + 1;
+                        int value2 = dpAmount[currentAmout];
+                        dpAmount[currentAmout] = Math.Min(value1, value2);
+                        isHasSolution = true;
+                    }
+
+                    if (false == isHasSolution)
+                        break;
+                }
+            }
+
+            return dpAmount[amount];
+        }
+
         public class CoinParam
         {
             public int[] Coins;
